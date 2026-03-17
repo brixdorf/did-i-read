ce6d56375d912a6d36204e734a59aaa515fa6a64
brixdorf <itsromit@protonmail.com>
2026-03-18 02:54:36 +0530

    Added dark mode to Homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,8 +2,8 @@ import Calendar from '../components/Calendar'
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gray-950 text-white p-4 pt-12">
-      <h1 className="text-7xl font-bold text-center mb-8 font-heading">
+    <div className="min-h-screen bg-gray-100 dark:bg-gray-950 p-4 pt-12">
+      <h1 className="text-7xl text-gray-900 dark:text-white font-bold text-center mb-8 font-heading">
         Did I Read Today?
       </h1>
       <div className="flex flex-col md:flex-row gap-8 max-w-5xl mx-auto">
