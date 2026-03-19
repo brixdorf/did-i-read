f4a3b807ed4cb622a0e9aa34d0fcab380e4cbd4b
brixdorf <itsromit@protonmail.com>
2026-03-19 22:05:21 +0530

    Created the basic structure of the database

---
+++ backend/database.js
@@ -0,0 +1,15 @@
+const Database = require('better-sqlite3')
+
+const db = new Database('database.db')
+
+db.exec(`
+    CREATE TABLE IF NOT EXISTS habits (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        type TEXT NOT NULL,
+        day INTEGER NOT NULL,
+        month INTEGER NOT NULL,
+        year INTEGER NOT NULL,
+    )
+    `)
+
+module.exports = db
