249ad640398ac3792a58757715f9d00f8b8f865f
brixdorf <itsromit@protonmail.com>
2026-03-22 20:17:33 +0530

    Design changes to header, bg and favicon

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -2,8 +2,8 @@ import Calendar from "../components/Calendar";
 
 export default function HomePage() {
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-950 p-4 pt-12">
-      <h1 className="text-7xl text-gray-900 dark:text-white font-bold text-center mb-8 font-heading">
+    <div className="min-h-screen rainbow-bg p-4 pt-12">
+      <h1 className="text-7xl heading-3d font-bold text-center mb-8 font-heading">
         Did I Read Today?
       </h1>
       <div className="flex flex-col md:flex-row gap-8 max-w-5xl mx-auto">
